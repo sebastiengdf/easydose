01156e666cbb166fe5e01587f91e42cc90e565e1
GIRIER DUFOURNIER Sebastien <sebastien.girierdufournier@srr.fr>
2023-10-05 11:10:55 +0400

    correction

--- ServeurDICOM/SCPSRV/exec/easyscpserver/model/Entity/DICOMDbManager.py
+++ ServeurDICOM/SCPSRV/exec/easyscpserver/model/Entity/DICOMDbManager.py
@@ -50,6 +50,7 @@ class DICOMDbManager(object):
         if patientpresent and PatientOld:
             myPatient=PatientOld; # on est sur le bon patient
             self.loggin.warning('Patient trouve !!!')
+            myPatient.datelastexam=date.today() 
         else: 
             self.loggin.warning('Nouveau Patient => insertion')                  
             myPatient = Patient(datelastexam=date.today(),havescanner=0,haveradio=0,havemammo=0,havenotes=0,nbdoses=0,sumhavealerte=0,nrdhavealerte=0,idregional="_",sex=self.manageDCMData.sex,uniteage=self.manageDCMData.unitageage,prenom=self.manageDCMData.patientprenom.encode('utf-8').decode('utf-8'), nom=self.manageDCMData.patientname.encode('utf-8').decode('utf-8'), numipp=self.manageDCMData.patientnumipp, datenaissance=self.manageDCMData.patientdatedenaissance, age=int(self.manageDCMData.patientage))
